0bc06e1c67afb27649d1d86ea32f4cc230b79d96
Lucca <62717844+Lucca19@users.noreply.github.com>
2022-03-17 21:02:41 -0300

    maior e menor idade

--- pesquisa.java
+++ pesquisa.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
+
 public class pesquisa {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         int i = 0;
 
         String ler[] = new String[10];
@@ -12,7 +13,7 @@ public class pesquisa {
         String vSexo[] = new String[1];
         String vOlhos[] = new String[1];
         String vCabelo[] = new String[1];
-        String vIdade[] = new String[1];
+        int vIdade[] = new int[1];
 
         int opcao = 0;
         while (opcao != 9) {
@@ -22,22 +23,20 @@ public class pesquisa {
             System.out.print("\n");
             System.out.print(" Escolha qual informação voce deseja saber: \n");
             System.out.print("\n");
-            System.out.print(" 2 - A maior idade dos habitantes \n");
-            System.out.print(" 3 - A maior idade dos habitantes \n");
-            System.out.print(" 4 - A menor idade dos habitantes \n");
-            System.out.print(" 5 - A maior idade das mulheres \n");
-            System.out.print(" 6 - A maior idade dos homens \n");
+            System.out.print(" 2 - A maior e menor idade dos habitantes \n");
+            System.out.print(" 3 - A maior idade das mulheres \n");
+            System.out.print(" 4 - A maior idade dos homens \n");
             System.out.print(
-                    " 7 - O número de mulheres cuja idade está entre 18 e 35 anos e que tenham olhos verdes e cabelos louros \n");
+                    " 5 - O número de mulheres cuja idade está entre 18 e 35 anos e que tenham olhos verdes e cabelos louros \n");
             System.out.print(
-                    " 8 - A porcentagem de mulheres cuja idade está entre 18 e 35 anos inclusive e que tenham olhos verdes e cabelos louros \n");
+                    " 6 - A porcentagem de mulheres cuja idade está entre 18 e 35 anos inclusive e que tenham olhos verdes e cabelos louros \n");
             System.out.print(
-                    " 9 - O número de homens cuja idade está entre 40 e 50 anos inclusive e que tenham olhos castanhos e cabelos pretos \n");
+                    " 7 - O número de homens cuja idade está entre 40 e 50 anos inclusive e que tenham olhos castanhos e cabelos pretos \n");
             System.out.print(
-                    " 10 - A porcentagem de homens cuja idade está entre 40 e 50 anos inclusive e que tenham olhos castanhos e cabelos pretos \n");
-            System.out.print(" 11 - A média de idade dos habitantes \n");
-            System.out.print(" 12 - O número total de habitantes \n");
-            System.out.print(" 13 - Sair \n");
+                    " 8 - A porcentagem de homens cuja idade está entre 40 e 50 anos inclusive e que tenham olhos castanhos e cabelos pretos \n");
+            System.out.print(" 10 - A média de idade dos habitantes \n");
+            System.out.print(" 11 - O número total de habitantes \n");
+            System.out.print(" 12 - Sair \n");
             System.out.print(
                     "_______________________________________________________________________________________________________________ \n");
             System.out.print("\n");
@@ -58,21 +57,37 @@ public class pesquisa {
                     String cabelo = input.next();
                     vCabelo[i] = cabelo;
 
-                    System.out.print(" 4 - Para digitar a sua idade \n");
-                    String idade = input.next();
+                    System.out.print(" 4 - Para digitar a sua idade: \n");
+                    int idade = input.nextInt();
                     vIdade[i] = idade;
 
-                    if (sexo.equals("-1")||olhos.equals("-1")||cabelo.equals("-1")||idade.equals("-1")) { // Este 'if' ira quebrar caso ele enconstre um '-1';
+                    if (sexo.equals("-1") || olhos.equals("-1") || cabelo.equals("-1")) { // Este 'if' ira quebrar caso
+                                                                                          // ele enconstre um '-1';
                         break;
                     }
+                    if (idade == -1) {
+                        break;
+                    }
+                    break;
 
                 case 2:
-
+                    int maior = 0;
+                    int menor = 300;
+                    for (int j=0; j<vIdade.length-1; j++){ 
+                        if(vIdade[j] > maior){
+                            maior = vIdade[j];
+                        }else if(vIdade[j] < menor){
+                            menor = vIdade[j];
+                        }
+                    }
+                    System.out.print("\nA Maior idade entre os habitantes é: " + maior);
+                    System.out.print("\nA Menor idade entre os habitantes é: " + menor);
                     break;
 
                 case 3:
 
-                    break;
+
+                break;
 
                 case 4:
 
@@ -115,6 +130,7 @@ public class pesquisa {
                     break;
 
                 default:
+                
                     break;
             }
         }
